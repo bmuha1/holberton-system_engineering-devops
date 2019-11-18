e4621006fa19e559cea0dea7040e4c3780598901
Brent Muha <800@holbertonschool.com>
2019-11-18 19:54:21 +0000

    Add text me

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-# puts ARGV[0].scan(/(?<=from:).*(?=\])/).join
 print ARGV[0].scan(/(?<=\[from:)(.*?)\]/).join + ','
 print ARGV[0].scan(/(?<=\[to:)(.*?)\]/).join + ','
 puts ARGV[0].scan(/(?<=\[flags:)(.*?)\]/).join
