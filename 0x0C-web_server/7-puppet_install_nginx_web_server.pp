e2109028e3cd92ac5694c215eeb6ed7b7623ed24
Brent Muha <800@holbertonschool.com>
2019-12-12 06:13:09 +0000

    Add puppet install nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,7 @@
 # Install and configure an Nginx server using Puppet
-package { 'nginx':
-  ensure => installed,
-}
-
-file { '/var/www/html/index.html':
-  content => 'Holberton School for the win!',
-}
-
-file_line { 'Redirect':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;',
-}
-
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
-}
+exec {'/usr/bin/env apt-get -y update':}
+exec {'/usr/bin/env apt-get -y install nginx':}
+exec {'/usr/bin/env ufw allow \'Nginx HTTP\'':}
+exec {'/usr/bin/env echo "Holberton School for the win!" > /var/www/html/index.nginx-debian.html':}
+exec {'/usr/bin/env sed -i "/listen 80 default_server/a rewrite ^/redirect_me https://www.youtube.com/watch?v=dQw4w9WgXcQ permanent;" /etc/nginx/sites-available/default':}
+exec {'/usr/bin/env service nginx restart':}
