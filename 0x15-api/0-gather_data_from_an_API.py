b2429e0ed7dd79eecb6c52e579168f65dd0fd964
Brent Muha <800@holbertonschool.com>
2020-02-03 18:22:58 +0000

    Add gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+For a given employee ID, return information about TODO list progress
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                     format(argv[1]))
+    name = r.json().get('name')
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                     format(argv[1]))
+    data = r.json()
+    done = total = 0
+
+    for task in data:
+        total += 1
+        if task.get('completed'):
+            done += 1
+
+    print('Employee {} is done with tasks({}/{}):'.format(name, done, total))
+
+    for task in data:
+        if task.get('completed'):
+            print('\t{}'.format(task.get('title')))
