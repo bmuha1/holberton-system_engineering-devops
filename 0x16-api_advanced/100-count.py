551c0cc69bdfe3a3b56dc4878217ecfd8b25a421
Brent Muha <800@holbertonschool.com>
2020-02-06 22:30:49 +0000

    Add count

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Query the Reddit API, parse the titles of all hot articles for a given
+subreddit, and print a sorted count of given keywords
+"""
+import requests
+import re
+
+
+def count_words(subreddit, word_list, hot_list=[], after=''):
+    try:
+        r = requests.get('https://www.reddit.com/r/{}/hot.json?after={}'.
+                         format(subreddit, after),
+                         headers={'User-Agent': 'custom'},
+                         allow_redirects=False)
+        if after is None:
+            dict = {}
+            for word in word_list:
+                dict[word] = str(hot_list).count(word)
+            for word in sorted(dict, key=dict.get, reverse=True):
+                if dict[word]:
+                    print('{}: {}'.format(word, dict[word]))
+            return hot_list
+        for thread in r.json().get('data').get('children'):
+            hot_list += [thread.get('data').get('title')]
+        after = r.json().get('data').get('after')
+        count_words(subreddit, word_list, hot_list, after)
+        return hot_list
+    except:
+        return None
