e97d066e63b8b07a6fbe388e127eb0345e59600f
Brent Muha <800@holbertonschool.com>
2020-02-06 19:18:08 +0000

    Fix style issues

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,11 +4,12 @@ Query the Reddit API and return the number of subscribers for a given subreddit
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     try:
         r = requests.get('https://www.reddit.com/r/{}/about.json'.
                          format(subreddit),
-                         headers={'User-Agent':'custom'},
+                         headers={'User-Agent': 'custom'},
                          allow_redirects=False)
         return r.json().get('data').get('subscribers')
     except:
