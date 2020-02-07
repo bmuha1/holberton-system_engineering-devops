4e725eb5851016a75a961fb698513e482d4dc085
Brent Muha <800@holbertonschool.com>
2020-02-07 02:30:42 +0000

    Try different counting algorithm

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -15,13 +15,18 @@ def count_words(subreddit, word_list, hot_list=[], after=''):
         if after is None:
             dict = {}
             for word in word_list:
-                dict[word] = str(hot_list).lower().count(word)
+                for hot_word in hot_list:
+                    if word == hot_word:
+                        if word not in dict:
+                            dict[word] = 1
+                        else:
+                            dict[word] += 1
             for word in sorted(dict, key=dict.get, reverse=True):
                 if dict[word]:
                     print('{}: {}'.format(word, dict[word]))
             return hot_list
         for thread in r.json().get('data').get('children'):
-            hot_list += [thread.get('data').get('title')]
+            hot_list += thread.get('data').get('title').lower().split()
         after = r.json().get('data').get('after')
         count_words(subreddit, word_list, hot_list, after)
         return hot_list
