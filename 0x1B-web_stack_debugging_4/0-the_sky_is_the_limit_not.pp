e60792bf41a07726778cd217295d70b434db9fe3
Brent Muha <800@holbertonschool.com>
2020-02-27 22:05:07 +0000

    Add paths

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # Increase the user limit from 15 to 4096
 exec { 'limit':
   command => "sed -i -e 's/15/4096/g' /etc/default/nginx",
-  path    => '/bin',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
 }
 
 exec { 'restart':
