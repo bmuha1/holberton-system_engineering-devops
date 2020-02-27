b9e763491b78d9316ec4c5996d5a1f70f6f4261f
Brent Muha <800@holbertonschool.com>
2020-02-27 22:07:31 +0000

    Restart nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # Increase the user limit from 15 to 4096
 exec { 'limit':
-  command => "sed -i -e 's/15/4096/g' /etc/default/nginx",
-  path    => ['/bin', '/usr/bin', '/usr/sbin'],
-}
-
-exec { 'restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d',
+  command => "sed -i -e 's/15/4096/g' /etc/default/nginx; service nginx restart",
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
